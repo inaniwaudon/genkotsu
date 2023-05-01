7aed47056520a78078bd6c171533dd412dd72d78
inaniwaudon <w.yuto0916@gmail.com>
2023-05-01 15:22:07 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/blog-template" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/genkotsu" : "",
 };
 
 module.exports = nextConfig;
