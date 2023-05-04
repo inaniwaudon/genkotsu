1c05f1f6f7fae7e56730f49bf8d299de84373890
inaniwaudon <w.yuto0916@gmail.com>
2023-05-05 03:22:17 +0900

    set trailingSlash true

--- next.config.js
+++ next.config.js
@@ -1,29 +1,30 @@
-const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? '/genkotsu' : '',
+  basePath: process.env.GITHUB_ACTIONS ? "/genkotsu" : "",
   webpack: (config) => {
-    const nextPublicDirPath = path.resolve(__dirname, 'public')
+    const nextPublicDirPath = path.resolve(__dirname, "public");
     config.plugins.push(
       new CopyPlugin({
-          patterns: [
-            {
-                from: './node_modules/gif.js/dist/gif.worker.js',
-                to: nextPublicDirPath
-            },
-            {
-                from: './node_modules/gif.js/dist/gif.worker.js.map',
-                to: nextPublicDirPath,
-            },
-          ],
+        patterns: [
+          {
+            from: "./node_modules/gif.js/dist/gif.worker.js",
+            to: nextPublicDirPath,
+          },
+          {
+            from: "./node_modules/gif.js/dist/gif.worker.js.map",
+            to: nextPublicDirPath,
+          },
+        ],
       })
-    )
+    );
 
-    return config
+    return config;
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
